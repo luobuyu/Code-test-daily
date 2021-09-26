2766aa244aed1b20ce23384f5ee9d0e53bc94b97
luobuyu <1528751774@qq.com>
2021-09-26 20:43:31 +0800

    178A星

---
+++ acwing/search/178.cpp
@@ -0,0 +1,164 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define lll long long
+namespace FAST_IO
+{
+
+    inline char nextChar()
+    {
+        static char buf[1000000], *p1 = buf, *p2 = buf;
+        return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 1000000, stdin), p1 == p2) ? EOF : *p1++;
+    }
+#define getch getchar
+    template <class T>
+    inline void read(T &x)
+    {
+        T flag = 1;
+        x = 0;
+        char ch = getch();
+        while (ch < '0' || ch > '9')
+        {
+            if (ch == '-')
+                flag = -1;
+            ch = getch();
+        }
+        while (ch >= '0' && ch <= '9')
+        {
+            x = (x << 3) + (x << 1) + (ch ^ 48), ch = getch();
+        }
+        x *= flag;
+    }
+
+    template <class T, class... _T>
+    inline void read(T &x, _T &...y)
+    {
+        return read(x), read(y...);
+    }
+
+    inline void print128(lll x)
+    {
+        if (x < 0)
+            putchar('-'), x = -x;
+        if (x > 9)
+            print128(x / 10);
+        putchar(x % 10 + '0');
+    }
+
+} // namespace FAST_IO
+using namespace std;
+using namespace FAST_IO;
+const ll mod = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll INF_LL = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-5;
+const int maxn = 1e3 + 10;
+const int maxm = 2e5 + 10;
+int t, n, m, k;
+int s, e;
+
+struct Edge
+{
+    int u, v, w, next;
+} edge[maxm];
+int head[maxn], rhead[maxn], cnt = 0;
+
+inline void addEdge(int u, int v, int w)
+{
+    edge[++cnt] = {u, v, w, head[u]};
+    head[u] = cnt;
+    edge[++cnt] = {v, u, w, rhead[v]};
+    rhead[v] = cnt;
+}
+int g[maxn], h[maxn];
+struct Node
+{
+    int u;
+    int dis;
+    bool operator<(const Node &p) const
+    {
+        return dis < p.dis;
+    }
+};
+
+bool vis[maxn];
+void dijkstra(int s)
+{
+    priority_queue<Node> q;
+    memset(h, 0x3f, sizeof(h));
+    memset(vis, false, sizeof(vis));
+    h[s] = 0;
+    q.push({s, 0});
+    while (q.size())
+    {
+        auto out = q.top();
+        q.pop();
+
+        int u = out.u;
+        if (vis[u])
+            continue;
+        vis[u] = true;
+
+        for (int i = rhead[u]; i; i = edge[i].next)
+        {
+            int v = edge[i].v;
+            if (h[v] > h[u] + edge[i].w)
+            {
+                h[v] = h[u] + edge[i].w;
+                q.push({v, -h[v]});
+            }
+        }
+    }
+}
+// A星 优先级队列里面放 f(x)
+// g
+int times[maxn];
+int astar()
+{
+    priority_queue<Node> q;
+    g[s] = 0;
+    q.push({s, -h[s]});
+    while (q.size())
+    {
+        auto out = q.top();
+        q.pop();
+        int u = out.u;
+        int gdis = -out.dis - h[u];
+        times[u]++;
+        if (times[e] == k)
+        {
+            return gdis;
+        }
+        for (int i = head[u]; i; i = edge[i].next)
+        {
+            int v = edge[i].v;
+            if (times[v] < k)
+            {
+                q.push({v, -(gdis + edge[i].w + h[v])});
+                // g[v] = g[u] + edge[i].w;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+// #define COMP_DATA
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    read(n, m);
+    for (int i = 1, u, v, w; i <= m; i++)
+    {
+        read(u, v, w);
+        addEdge(u, v, w);
+    }
+    read(s, e, k);
+    if (s == e)
+        k++;
+    dijkstra(e);
+    // cout << h[s] << endl;
+    int ret = astar();
+    cout << ret << endl;
+    return 0;
+}
---
+++ acwing/search/178std.cpp
@@ -0,0 +1,107 @@
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+#define x first
+#define y second
+
+using namespace std;
+
+typedef pair<int, int> PII;
+typedef pair<int, PII> PIII;
+
+const int N = 1010, M = 200010;
+
+int n, m, S, T, K;
+int h[N], rh[N], e[M], w[M], ne[M], idx;
+int dist[N], cnt[N];
+bool st[N];
+
+void add(int h[], int a, int b, int c)
+{
+    e[idx] = b, w[idx] = c, ne[idx] = h[a], h[a] = idx++;
+}
+
+void dijkstra()
+{
+    priority_queue<PII, vector<PII>, greater<PII>> heap;
+    heap.push({0, T});
+
+    memset(dist, 0x3f, sizeof dist);
+    dist[T] = 0;
+
+    while (heap.size())
+    {
+        auto t = heap.top();
+        heap.pop();
+
+        int ver = t.y;
+        if (st[ver])
+            continue;
+        st[ver] = true;
+
+        for (int i = rh[ver]; ~i; i = ne[i])
+        {
+            int j = e[i];
+            if (dist[j] > dist[ver] + w[i])
+            {
+                dist[j] = dist[ver] + w[i];
+                heap.push({dist[j], j});
+            }
+        }
+    }
+}
+
+int astar()
+{
+    priority_queue<PIII, vector<PIII>, greater<PIII>> heap;
+    heap.push({dist[S], {0, S}});
+
+    while (heap.size())
+    {
+        auto t = heap.top();
+        heap.pop();
+
+        int ver = t.y.y, distance = t.y.x;
+        cnt[ver]++;
+        if (cnt[T] == K)
+            return distance;
+
+        for (int i = h[ver]; ~i; i = ne[i])
+        {
+            int j = e[i];
+            if (cnt[j] < K)
+                heap.push({distance + w[i] + dist[j], {distance + w[i], j}});
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    // #define COMP_DATA
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    scanf("%d%d", &n, &m);
+    memset(h, -1, sizeof h);
+    memset(rh, -1, sizeof rh);
+
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        add(h, a, b, c);
+        add(rh, b, a, c);
+    }
+    scanf("%d%d%d", &S, &T, &K);
+    if (S == T)
+        K++;
+
+    dijkstra();
+    printf("%d\n", astar());
+
+    return 0;
+}
