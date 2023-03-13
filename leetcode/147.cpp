5dcb2efcabe763369d7da2e1a21fdfe944333368
luobuyu <1528751774@qq.com>
2023-03-13 21:09:13 +0800

    ..

---
+++ leetcode/147.cpp
@@ -0,0 +1,139 @@
+// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include "head.h"
+#define ll long long
+#define lll long long
+#define PII pair<int, int>
+namespace FAST_IO
+{
+
+    inline char nextChar()
+    {
+        static char buf[1000000], *p1 = buf, *p2 = buf;
+        return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 1000000, stdin), p1 == p2) ? EOF : *p1++;
+    }
+#define getch getchar
+    template <class T>
+    inline void read(T &x)
+    {
+        T flag = 1;
+        x = 0;
+        char ch = getch();
+        while (ch < '0' || ch > '9')
+        {
+            if (ch == '-')
+                flag = -1;
+            ch = getch();
+        }
+        while (ch >= '0' && ch <= '9')
+        {
+            x = (x << 3) + (x << 1) + (ch ^ 48), ch = getch();
+        }
+        x *= flag;
+    }
+
+    template <class T, class... _T>
+    inline void read(T &x, _T &...y)
+    {
+        return read(x), read(y...);
+    }
+
+    inline void print128(lll x)
+    {
+        if (x < 0)
+            putchar('-'), x = -x;
+        if (x > 9)
+            print128(x / 10);
+        putchar(x % 10 + '0');
+    }
+
+} // namespace FAST_IO
+using namespace std;
+using namespace FAST_IO;
+const ll mod = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll INF_LL = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-5;
+int t, n, m, k;
+class Solution
+{
+public:
+    ListNode *insertionSortList(ListNode *head)
+    {
+        // 升序
+        ListNode *vir_head = new ListNode;
+        vir_head->next = head;
+        ListNode *head2 = new ListNode;
+        head2->next = nullptr;
+        // 找最小的
+        ListNode *pre, *cur;
+        ListNode *min_pre, *min_cur;
+        int minx;
+        while (true)
+        {
+            minx = 10000;
+            pre = vir_head;
+            cur = vir_head->next;
+            if (cur == nullptr)
+                return head2->next;
+            while (cur)
+            {
+                if (cur->val < minx)
+                {
+                    min_pre = pre;
+                    min_cur = cur;
+                    minx = cur->val;
+                }
+                pre = cur;
+                cur = cur->next;
+            }
+            // 删除
+            min_pre->next = min_cur->next;
+            // 找插入位置
+            pre = head2;
+            cur = head2->next;
+            while (cur)
+            {
+                if (cur->val >= min_cur->val)
+                {
+                    pre->next = min_cur;
+                    min_cur->next = cur;
+                    break;
+                }
+                pre = cur;
+                cur = cur->next;
+            }
+            if (cur == nullptr)
+            {
+                pre->next = min_cur;
+                min_cur->next = nullptr;
+            }
+        }
+    }
+};
+
+int main()
+{
+// #define COMP_DATA
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    Solution solution;
+    ListNode *head = new ListNode;
+    head->next = nullptr;
+    ListNode *tail = head;
+    read(n);
+    for (int i = 0, x; i < n; ++i)
+    {
+        read(x);
+        ListNode *p = new ListNode;
+        p->val = x;
+        p->next = nullptr;
+        tail->next = p;
+        tail = p;
+    }
+    solution.insertionSortList(head->next);
+    return 0;
+}
