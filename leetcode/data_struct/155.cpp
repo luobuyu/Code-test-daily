f607bc9f6aaa717cefd5bc11412c572ff0e8a5b3
luobuyu <1528751774@qq.com>
2023-04-11 14:58:29 +0800

    ..

---
+++ leetcode/data_struct/155.cpp
@@ -0,0 +1,119 @@
+// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include "head.h"
+#define ll long long
+#define lll long long
+#define PII pair<int, int>
+namespace FAST_IO
+{
+
+    inline char nextChar()
+    {
+        static char buf[1000000], *p1 = buf, *p2 = buf;
+        return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 1000000, stdin), p1 == p2) ? EOF : *p1++;
+    }
+#define getch getchar
+    template <class T>
+    inline void read(T &x)
+    {
+        T flag = 1;
+        x = 0;
+        char ch = getch();
+        while (ch < '0' || ch > '9')
+        {
+            if (ch == '-')
+                flag = -1;
+            ch = getch();
+        }
+        while (ch >= '0' && ch <= '9')
+        {
+            x = (x << 3) + (x << 1) + (ch ^ 48), ch = getch();
+        }
+        x *= flag;
+    }
+
+    template <class T, class... _T>
+    inline void read(T &x, _T &...y)
+    {
+        return read(x), read(y...);
+    }
+
+    inline void print128(lll x)
+    {
+        if (x < 0)
+            putchar('-'), x = -x;
+        if (x > 9)
+            print128(x / 10);
+        putchar(x % 10 + '0');
+    }
+
+} // namespace FAST_IO
+using namespace std;
+using namespace FAST_IO;
+const ll mod = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll INF_LL = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-5;
+int t, n, m, k;
+class MinStack
+{
+public:
+    const static int maxn = 1e5 + 10;
+    const static int maxm = 1e5 + 10;
+    const static int INF = 0x3f3f3f3f;
+    static void optimize_cpp_stdio() { ios::sync_with_stdio(false), cin.tie(0); }
+    // 辅助栈，单调减
+    stack<int> s;
+    stack<int> st;
+    MinStack()
+    {
+        optimize_cpp_stdio();
+    }
+
+    void push(int val)
+    {
+        s.push(val);
+        if (st.empty() || st.top() >= val)
+            st.push(val);
+    }
+
+    void pop()
+    {
+        int tmp = s.top();
+        s.pop();
+        if (st.size() && tmp == st.top())
+            st.pop();
+    }
+
+    int top()
+    {
+        return s.top();
+    }
+
+    int getMin()
+    {
+        return st.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+int main()
+{
+// #define COMP_DATA
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    MinStack solution;
+
+    return 0;
+}
---
+++ leetcode/data_struct/162.cpp
@@ -0,0 +1,118 @@
+// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include "head.h"
+#define ll long long
+#define lll long long
+#define PII pair<int, int>
+namespace FAST_IO
+{
+
+    inline char nextChar()
+    {
+        static char buf[1000000], *p1 = buf, *p2 = buf;
+        return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 1000000, stdin), p1 == p2) ? EOF : *p1++;
+    }
+#define getch getchar
+    template <class T>
+    inline void read(T &x)
+    {
+        T flag = 1;
+        x = 0;
+        char ch = getch();
+        while (ch < '0' || ch > '9')
+        {
+            if (ch == '-')
+                flag = -1;
+            ch = getch();
+        }
+        while (ch >= '0' && ch <= '9')
+        {
+            x = (x << 3) + (x << 1) + (ch ^ 48), ch = getch();
+        }
+        x *= flag;
+    }
+
+    template <class T, class... _T>
+    inline void read(T &x, _T &...y)
+    {
+        return read(x), read(y...);
+    }
+
+    inline void print128(lll x)
+    {
+        if (x < 0)
+            putchar('-'), x = -x;
+        if (x > 9)
+            print128(x / 10);
+        putchar(x % 10 + '0');
+    }
+
+} // namespace FAST_IO
+using namespace std;
+using namespace FAST_IO;
+const ll mod = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+const ll INF_LL = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-5;
+int t, n, m, k;
+class Solution
+{
+public:
+    const static int maxn = 1e5 + 10;
+    const static int maxm = 1e5 + 10;
+    const static int INF = 0x3f3f3f3f;
+    static void optimize_cpp_stdio() { ios::sync_with_stdio(false), cin.tie(0); }
+
+    bool check(int n, int mid, vector<int> &nums)
+    {
+        if (mid == 0 && nums[mid] > nums[mid + 1])
+            return true;
+        if (mid == n - 1 && nums[mid] > nums[mid - 1])
+            return true;
+        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+            return true;
+        return false;
+    }
+    int findPeakElement(vector<int> &nums)
+    {
+        optimize_cpp_stdio();
+        int n = nums.size();
+        if (n == 1)
+            return 0;
+        if (n == 2)
+            return nums[0] > nums[1] ? 0 : 1;
+        int l = 0, r = n - 1, mid, ans;
+        while (l <= r)
+        {
+            mid = l + (r - l >> 1);
+            if (check(n, mid, nums))
+            {
+                ans = mid;
+                break;
+            }
+            else if (nums[mid] < nums[mid + 1])
+                l = mid + 1;
+            else if (nums[mid] < nums[mid - 1])
+                r = mid - 1;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+// #define COMP_DATA
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    Solution solution;
+    vector<int> a;
+    while (cin >> k)
+    {
+        a.emplace_back(k);
+    }
+    cout << solution.findPeakElement(a) << endl;
+    return 0;
+}
