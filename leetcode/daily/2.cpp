3456555889fe502308a734ba1e66c117f3ba312a
luobuyu <1528751774@qq.com>
2023-07-02 22:02:10 +0800

    2023年7月2日22:02:08

--- leetcode/daily/2.cpp
+++ leetcode/daily/2.cpp
@@ -81,28 +81,50 @@ int init = []
     cin.tie(nullptr);
     cout.tie(nullptr);
     /*************************************/
-
-    int dp[100000][2] = {};
-    int v, ans;
-    string s;
-    for (; getline();)
+    static const int maxn = 100 + 1;
+    int a[maxn];
+    int b[maxn];
+    int l1, l2;
+    while (true)
     {
-        for (int i = 0; read(v); ++i)
+        if (!getline())
+            break;
+        int x;
+
+        while (read(x))
         {
-            if (i == 0)
-            {
-                dp[0][0] = v;
-                dp[0][1] = 0;
-                ans = v;
-            }
-            else
-            {
-                dp[i][0] = max(dp[i - 1][0] + v, v);
-                dp[i][1] = max(dp[i - 1][0], dp[i - 1][1] + v);
-                ans = max(ans, max(dp[i][0], dp[i][1]));
-            }
+            a[l1++] = x;
         }
-        cout << ans << endl;
+        getline();
+        while (read(x))
+        {
+            b[l2++] = x;
+        }
+        int i = 0, j = 0;
+        int val = 0, add = 0;
+        int aa, bb;
+        cout << "[";
+        while (i < l1 || j < l2 || add)
+        {
+            if (i != 0 && j != 0)
+                cout << ",";
+            if (i < l1)
+                aa = a[i];
+            else
+                aa = 0;
+            if (j < l2)
+                bb = b[i];
+            else
+                bb = 0;
+            val = aa + bb + add;
+            cout << val % 10;
+            add = val / 10;
+            if (i < l1)
+                ++i;
+            if (j < l2)
+                ++j;
+        }
+        cout << "]\n";
     }
     exit(0);
     return 0;
@@ -118,33 +140,11 @@ auto optimize_cpp_stdio = []()
 class Solution
 {
 public:
-    const static int maxn = 5e2 + 10;
+    const static int maxn = 1e5 + 10;
     const static int maxm = 1e5 + 10;
     const int INF = 0x3f3f3f3f;
-    int dp[maxn][maxn];
-    int minDistance(string word1, string word2)
+    int minSubarray(vector<int> &nums, int p)
     {
-        memset(dp, 0x3f, sizeof(dp));
-        int n = word1.size(), m = word2.size();
-        for (int j = 0; j <= m; ++j)
-            dp[0][j] = j;
-        for (int i = 0; i <= n; ++i)
-            dp[i][0] = i;
-        for (int i = 1; i <= n; ++i)
-        {
-            for (int j = 1; j <= m; ++j)
-            {
-                if (word1[i - 1] == word2[j - 1])
-                {
-                    dp[i][j] = dp[i - 1][j - 1];
-                }
-                else
-                {
-                    dp[i][j] = min(dp[i][j], min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1);
-                }
-            }
-        }
-        return dp[n][m];
     }
 };
 
