b82cc4e378ca60129e4f1dc7fd9f856ef0726fbb
luobuyu <1528751774@qq.com>
2023-09-07 11:49:58 +0800

    2023年9月7日

---
+++ leetcode/data_struct/128.cpp
@@ -0,0 +1,181 @@
+// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include "head.h"
+#define ll long long
+#define lll long long
+#define PII pair<int, int>
+namespace FAST_IO
+{
+    static string buf_line;
+    static int _i;
+    static int _n;
+    static char _ch;
+    template <class T>
+    inline bool read(T &x)
+    {
+        T flag = 1;
+        x = 0;
+        if (_i >= _n)
+            return false;
+        _ch = buf_line[_i++];
+        while (_ch < '0' || _ch > '9')
+        {
+            if (_ch == '-')
+                flag = -1;
+            _ch = buf_line[_i++];
+        }
+        while (_ch >= '0' && _ch <= '9')
+        {
+            x = (x << 3) + (x << 1) + (_ch ^ 48), _ch = buf_line[_i++];
+        }
+        x *= flag;
+        return true;
+    }
+
+    template <class T, class... _T>
+    inline bool read(T &x, _T &...y)
+    {
+        return read(x), read(y...);
+    }
+
+    inline bool getline()
+    {
+        if (!getline(cin, buf_line))
+            return false;
+        _i = 0, _n = buf_line.length();
+        return true;
+    }
+    template <class T>
+    inline void show(T *a, int n)
+    {
+        cout << "[";
+        for (int i = 0; i < n; ++i)
+        {
+            if (i != 0)
+                cout << ",";
+            cout << a[i];
+        }
+        cout << "]";
+    }
+
+    bool endofl()
+    {
+        if (_i >= _n)
+            return true;
+        if (_i == 0)
+            return false;
+        if (buf_line[_i - 1] == ']')
+        {
+            _i++;
+            return true;
+        }
+        return false;
+    }
+
+    template <class T, std::size_t Num>
+    inline void show(T a[][Num], int n, int m)
+    {
+        cout << "[";
+        for (int i = 0; i < n; ++i)
+        {
+            if (i != 0)
+                cout << ",";
+            show(a[i], m);
+        }
+        cout << "]";
+    }
+
+} // namespace FAST_IO
+using namespace FAST_IO;
+
+// int init = []
+// {
+//     /*********** fast_read ***************/
+//     freopen("user.out", "w", stdout);
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     /*************************************/
+
+//     while (true)
+//     {
+//         if (!getline())
+//             break;
+
+//         getline();
+//     }
+//     exit(0);
+//     return 0;
+// }();
+
+auto optimize_cpp_stdio = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
+class Solution
+{
+public:
+    const static int maxn = 1e5 + 10;
+    const static int maxm = 1e5 + 10;
+    const int INF = 0x3f3f3f3f;
+    struct UF
+    {
+        unordered_map<int, int> fa;
+        unordered_map<int, int> cnt;
+        void insert(int u)
+        {
+            fa[u] = u;
+            cnt[u] = 1;
+        }
+        int unite(int u, int v)
+        {
+            int up = find(u), vp = find(v);
+            if (up != vp)
+            {
+                fa[up] = vp;
+                cnt[vp] += cnt[up];
+            }
+            return cnt[vp];
+        }
+
+        int find(int u) { return fa[u] == u ? u : fa[u] = find(fa[u]); }
+
+        bool exist(int u) { return fa.count(u); }
+    };
+    int longestConsecutive(vector<int> &nums)
+    {
+        int size = 0;
+        UF uf;
+        for (auto &x : nums)
+        {
+            uf.insert(x);
+        }
+
+        int max_size = 0;
+        for (auto &x : nums)
+        {
+            if (uf.exist(x + 1))
+            {
+                max_size = max(max_size, uf.unite(x, x + 1));
+            }
+        }
+        return max_size;
+    }
+};
+
+int t, n, m, k;
+int main()
+{
+// #define COMP_DATA
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    Solution solution;
+
+    return 0;
+}
---
+++ leetcode/data_struct/2594.cpp
@@ -0,0 +1,179 @@
+// #pragma GCC optimize(2)
+#include <bits/stdc++.h>
+#include "head.h"
+#define ll long long
+#define lll long long
+#define PII pair<int, int>
+namespace FAST_IO
+{
+    static string buf_line;
+    static int _i;
+    static int _n;
+    static char _ch;
+    template <class T>
+    inline bool read(T &x)
+    {
+        T flag = 1;
+        x = 0;
+        if (_i >= _n)
+            return false;
+        _ch = buf_line[_i++];
+        while (_ch < '0' || _ch > '9')
+        {
+            if (_ch == '-')
+                flag = -1;
+            _ch = buf_line[_i++];
+        }
+        while (_ch >= '0' && _ch <= '9')
+        {
+            x = (x << 3) + (x << 1) + (_ch ^ 48), _ch = buf_line[_i++];
+        }
+        x *= flag;
+        return true;
+    }
+
+    template <class T, class... _T>
+    inline bool read(T &x, _T &...y)
+    {
+        return read(x), read(y...);
+    }
+
+    inline bool getline()
+    {
+        if (!getline(cin, buf_line))
+            return false;
+        _i = 0, _n = buf_line.length();
+        return true;
+    }
+    template <class T>
+    inline void show(T *a, int n)
+    {
+        cout << "[";
+        for (int i = 0; i < n; ++i)
+        {
+            if (i != 0)
+                cout << ",";
+            cout << a[i];
+        }
+        cout << "]";
+    }
+
+    bool endofl()
+    {
+        if (_i >= _n)
+            return true;
+        if (_i == 0)
+            return false;
+        if (buf_line[_i - 1] == ']')
+        {
+            _i++;
+            return true;
+        }
+        return false;
+    }
+
+    template <class T, std::size_t Num>
+    inline void show(T a[][Num], int n, int m)
+    {
+        cout << "[";
+        for (int i = 0; i < n; ++i)
+        {
+            if (i != 0)
+                cout << ",";
+            show(a[i], m);
+        }
+        cout << "]";
+    }
+
+} // namespace FAST_IO
+using namespace FAST_IO;
+
+// int init = []
+// {
+//     /*********** fast_read ***************/
+//     freopen("user.out", "w", stdout);
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+//     /*************************************/
+
+//     while (true)
+//     {
+//         if (!getline())
+//             break;
+
+//         getline();
+//     }
+//     exit(0);
+//     return 0;
+// }();
+
+auto optimize_cpp_stdio = []()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
+class Solution
+{
+public:
+    const static int maxn = 1e5 + 10;
+    const static int maxm = 1e5 + 10;
+    const int INF = 0x3f3f3f3f;
+    bool check(long long mid, vector<int> &nums, int cars)
+    {
+        long long cnt = 0;
+        for (int i = 0; i <= 100; ++i)
+        {
+            int x = nums[i];
+            if (x != 0)
+            {
+                cnt += x * sqrt(mid / i);
+            }
+        }
+        return cnt >= cars;
+    }
+    long long repairCars(vector<int> &ranks, int cars)
+    {
+        int n = ranks.size();
+        int maxx = -1;
+        vector<int> cnt(100 + 1, 0);
+        for (auto &x : ranks)
+        {
+            maxx = max(maxx, x);
+            cnt[x]++;
+        }
+        long long num = cars / n + 1;
+        long long l = 0, r = 1ll * maxx * num * num, ans = r;
+        while (l <= r)
+        {
+            long long mid = (l + r) >> 1;
+            if (check(mid, cnt, cars))
+            {
+                ans = mid;
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+int t, n, m, k;
+int main()
+{
+// #define COMP_DATA
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    Solution solution;
+    vector<int> a = {1, 1, 3, 3};
+    cout << solution.repairCars(a, 74) << endl;
+    return 0;
+}
